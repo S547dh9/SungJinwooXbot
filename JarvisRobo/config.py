9003684ea7c4fe76d6382a5cfc71af3737c22735
S547dh9 <84910430+S547dh9@users.noreply.github.com>
2024-07-02 00:12:15 +0530

    Update config.py

--- JarvisRobo/config.py
+++ JarvisRobo/config.py
@@ -6,7 +6,7 @@ class Config(object):
 
     API_ID = "26778642" # integer value, dont use ""
     API_HASH = "6ad48c8acff7a66f149f7203eaa73503"
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "6639557406:AAFkqbJnlRUkrNKkEWU2Mi8p-XInra2Q5VQ"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = "5702598840" # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
     SUPPORT_CHAT = "Flex_Support_Chat"  # Your own group for support, do not add the @
